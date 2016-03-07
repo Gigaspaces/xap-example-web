267033ed6709af450845f0a6485d55e30bba387c
tamirt <tamirt@gigaspaces,com>
2016-03-07 16:04:38 +0200

    GS-12768 Mavenize all the web examples. Reviewer:Kobi

--- examples/web/session/build.sh
+++ examples/web/session/build.sh
@@ -1,5 +1,43 @@
 #!/bin/bash
 
-. `dirname $0`/../../../bin/setenv.sh
+DIR_NAME=$(dirname ${BASH_SOURCE[0]})
+. $DIR_NAME/../../../bin/setenv.sh
 
-"$JAVACMD" ${XAP_OPTIONS} -classpath "${SIGAR_JARS}":"${GS_JARS}":"${ANT_JARS}":"${JAVA_HOME}/lib/tools.jar" org.apache.tools.ant.Main $1
+if [ "${M2_HOME}" = "" ] ; then
+    M2_HOME="${XAP_HOME}/tools/maven/apache-maven-3.0.2"; export M2_HOME #needed to upgraded to 3.2.5
+fi
+
+if [ $1 = "clean" ]; then
+  (cd $DIR_NAME;
+  ${M2_HOME}/bin/mvn clean; )
+
+elif [ $1 = "compile" ]; then
+  (cd $DIR_NAME;
+  ${M2_HOME}/bin/mvn compile; )
+
+elif [ $1 = "package" ]; then
+  (cd $DIR_NAME;
+  ${M2_HOME}/bin/mvn package; )
+
+elif [ $1 = "deploy" ]; then
+  ${XAP_HOME}/bin/gs.sh deploy-space -cluster schema=partitioned-sync2backup total_members=2,1 mySpace
+  ${XAP_HOME}/bin/gs.sh deploy $DIR_NAME/target/HttpSession.war
+
+elif [ $1 = "undeploy" ]; then
+  ${XAP_HOME}/bin/gs.sh undeploy HttpSession
+  ${XAP_HOME}/bin/gs.sh undeploy mySpace
+
+
+else
+  echo ""
+  echo "Error: Invalid input command: $1 "
+  echo ""
+  echo "The available commands are:"
+  echo ""
+  echo "clean                    --> Cleans all output dirs"
+  echo "compile                  --> Builds all; don't create JARs"
+  echo "package                  --> Builds the distribution"
+  echo "deploy                   --> Deploys the web app on the service grid"
+  echo "undeploy                 --> Undeploys the web app from the service grid"
+  echo ""
+fi
--- examples/web/space-access/build.bat
+++ examples/web/space-access/build.bat
@@ -1,5 +1,53 @@
 @echo off
 
-@call "%~dp0\..\..\..\bin\setenv.bat"
+call "%~dp0..\..\..\bin\setenv.bat"
+set DIR_NAME=%~dp0
+if not defined M2_HOME set M2_HOME=%~dp0..\..\tools\maven\apache-maven-3.0.2 rem needed to upgraded to 3.2.5
 
-@%JAVACMD% %XAP_OPTIONS% -classpath %SIGAR_JARS%;%GS_JARS%;%ANT_JARS%;"%JAVA_HOME%/lib/tools.jar" org.apache.tools.ant.Main %1
+if "%1" == "clean" (
+  cd %DIR_NAME%
+  call "%M2_HOME%\bin\mvn.bat" clean
+  cd %CD%
+) else (
+	if "%1" == "compile" (
+	    cd %DIR_NAME%
+		call "%M2_HOME%\bin\mvn.bat" compile
+		cd %CD%
+	) else (
+		if "%1" == "package" (
+		    cd %DIR_NAME%		
+			call "%M2_HOME%\bin\mvn.bat" package
+			cd %CD%
+		) else (
+			if "%1" == "deploy" (
+				cd %DIR_NAME%		
+				call "%XAP_HOME%\bin\gs.bat" deploy-space -cluster schema=partitioned-sync2backup total_members=2,1 mySpace
+				call "%XAP_HOME%\bin\gs.bat" deploy target\SpaceAccess.war
+				cd %CD%
+			) else (
+				if "%1" == "undeploy" (
+					cd %DIR_NAME%
+				    call "%XAP_HOME%\bin\gs.bat" undeploy SpaceAccess
+				    call "%XAP_HOME%\bin\gs.bat" undeploy mySpace
+					cd %CD%
+				) else (
+                    echo.
+                    echo Error: Invalid input command %1
+                    echo.
+                    echo The available commands are:
+                    echo.
+                    echo clean                    --^> Cleans all output dirs
+                    echo compile                  --^> Builds all; don't create WAR file
+                    echo package                  --^> Builds the distribution
+                    echo deploy                   --^> Deploys the web app on the service grid
+                    echo undeploy                 --^> Undeploys the web app from the service grid
+                    echo.
+					)	
+				)
+			)
+		)
+	)
+)
+ 
+
+
